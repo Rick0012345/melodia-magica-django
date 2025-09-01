099c16568acd11d32f2a99ce501e4d2212c28521
Patrick Oliveira <po44243@gmail.com>
2025-09-01 19:45:37 -0300

    separei os ursinhos, corrigi as fotos pra aparecerem apenas na tela de contato, e adicionei um video maneiro de plano de fundo pra apresentação

--- melodiaMagica/urls.py
+++ melodiaMagica/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,7 @@ urlpatterns = [
     path('chatbot/', include('chatbot.urls')),
     path('', include('core.urls')),
 ]
+
+# Servir arquivos estáticos em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
